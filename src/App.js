48c97ac840f5f01fd389e6fd83ed75a42bc53aef
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-11-26 11:55:42 -0500

    Add footer, hover text, edit tracker

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import './styles.css';
 
-function Background() {
+function Body() {
   return (
     <body>
       <Title/>
+      <EditTracker />
       <Canvas/>
+      <VDivider/>
+      <Footer/>
     </body>
   )
 }
 
 function Title() {
+    return <h1 className='title'>Braeden Meikle</h1>
+}
+
+const lastEditDate = 'Tuesday, November 26'
+
+function EditTracker() {
   return (
-    <h1 className='title'>Braeden Meikle</h1>
+    <div className='edit-tracker'>
+      <p className='last-seen'>Last seen:</p>
+      <p className='date'>{lastEditDate}</p>
+    </div>
   )
 }
 
 function Canvas() {
-  return (
-    <div className='canvas'>
+  return <div className='canvas'></div>
+}
 
+function VDivider() {
+  return <div className='vdiv'></div>
+}
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p className='footer-txt'>Created November 26, 2024</p>
+      <p className='hdiv'>|</p>
+      <span className='footer-txt' id='phone'></span>
+      <p className='hdiv'>|</p>
+      <span className='footer-txt' id='email'></span>
+      <p className='hdiv'>|</p>
+      <span className='footer-txt' id='linkedin'></span>
+      <p className='hdiv'>|</p>
+      <span className='footer-txt' id='github'></span>
     </div>
   )
 }
 
 function App() {
-  return (
-    <Background/>
-  )
+  return <Body/>
 }
 
 // Royal Flushes: 0
