4d14e5041124761f2002c4c1c63d9332572db622
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-11-30 12:02:31 -0500

    Add accordion menu for thoughts

--- src/sections/thoughts.js
+++ src/sections/thoughts.js
@@ -1,7 +1,40 @@
 
 
+const thoughts = [
+  {
+    title: 'Swift is the best programming language out there',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'Why I prefer force-unwrapping to nil-coalescing',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+]
+
 function Thoughts() {
-  return <div></div>
+  const contentMaxHeight = '500px'
+
+  function displayContent(index) {
+    const panels = document.getElementsByClassName('panel')
+    const chevrons = document.getElementsByClassName('chevron')
+    chevrons[index].classList.toggle('rotate')
+    panels[index].style.maxHeight = panels[index].style.maxHeight === contentMaxHeight ? '0px' : contentMaxHeight
+    panels[index].classList.toggle('drop')
+  }
+
+  return (
+    <div style={{display: 'grid', justifyContent: 'center'}}>
+      {thoughts.map((thought, index) => <div>
+        <button className='drop-down' onClick={() => displayContent(index)}>
+          <p>{thought.title}</p>
+          <span className='chevron' style={{marginLeft:'auto'}}>&rsaquo;</span>
+        </button>
+        <div class='panel'>
+          <p style={{color: 'white'}}>{thought.content}</p>
+        </div>
+      </div>)}
+    </div>
+  )
 }
 
 export { Thoughts }
