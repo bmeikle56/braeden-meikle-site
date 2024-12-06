ed03a3de8877eceb66866b86c2bf8b89ac0fb581
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-12-05 22:17:46 -0500

    Update env var name

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -20,7 +20,7 @@ function Body() {
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
-    const baseURL = `${process.env.SERVER_BASE_URL}/getUnread`
+    const baseURL = `${process.env.REACT_APP_SERVER_BASE_URL}/getUnread`
     const user = Math.floor(Math.random()*2) === 1 ? 'user7' : 'user2'
     const service = '/getUnread'
     const url =  `${baseURL}${service}?user=${user}`
