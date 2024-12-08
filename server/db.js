092ba0c7d3d4a248eebf35586d8cdbec14900af0
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-12-08 14:03:15 -0500

    Comment postgres code out

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
 
-import { Pool } from { pg }
+// import { Pool } from { pg }
 
-// Postgres instance
-const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT, // 5432
-})
+// // Postgres instance
+// const db = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT, // 5432
+// })
 
-export { db }
+// export { db }
