fcf560cb3f0b7367069341744c73b85171871951
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-11 18:54:48 -0400

    Fix link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
-import { txtCol } from "../styles/colors"
+import { txtCol } from '../styles/colors'
+import { useNavigate } from 'react-router-dom'
 
 const footerDivs = [
-  {txt: 'Created November 26, 2024'},
-  {txt: 'Email', link: 'mailto:braedenmeikle@gmail.com', icon: 'fa fa-envelope'},
-  {txt: 'LinkedIn', link: 'https://www.linkedin.com/in/bmeikle3/', icon: 'fa fa-linkedin-square'},
-  {txt: 'GitHub', link: 'https://github.com/bmeikle56', icon: 'fa fa-github'},
-  {txt: 'Projects', link: 'https://braedenmeikle.dev/projects', icon: 'fa fa-folder'},
+  {txt: 'Created November 26, 2024', nav: false},
+  {txt: 'Email', link: 'mailto:braedenmeikle@gmail.com', icon: 'fa fa-envelope', nav: false},
+  {txt: 'LinkedIn', link: 'https://www.linkedin.com/in/bmeikle3/', icon: 'fa fa-linkedin-square', nav: false},
+  {txt: 'GitHub', link: 'https://github.com/bmeikle56', icon: 'fa fa-github', nav: false},
+  {txt: 'Projects', link: 'https://braedenmeikle.dev/projects', icon: 'fa fa-folder', nav: true},
 ]
 
 const iconStyle = {
@@ -14,17 +15,24 @@ const iconStyle = {
 }
 
 function Footer() {
+  const navigate = useNavigate()
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}> 
       <div className='footer'>
         {footerDivs.map((footerDiv, i) => {
           return <div key={i} style={{display: 'flex', alignItems:'center'}}>
             <i className={footerDiv.icon} style={iconStyle}></i>
+            {footerDiv.nav && 
+              <button style={{textDecoration:'none', background: 'transparent'}} onClick={() => navigate('/projects')}>
+                <p className='footer-txt' style={{paddingLeft:'4px'}}>{footerDiv.txt}</p>
+              </button>}
             {footerDiv.link ? 
               <a rel='noreferrer' target='_blank' href={footerDiv.link} style={{textDecoration:'none'}}><p className='footer-txt' style={{paddingLeft:'4px'}}>{footerDiv.txt}</p></a>
               : <p className='footer-txt'>{footerDiv.txt}</p>}
           </div>
         })}
+        
       </div>
     </div>
   )
