067bfa13b6cf8db954f048c818fbcbc78bd5278e
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-11 19:01:06 -0400

    Style projects link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { txtCol } from '../styles/colors'
+import { txtCol, specialColor } from '../styles/colors'
 import { useNavigate } from 'react-router-dom'
 
 const footerDivs = [
@@ -6,13 +6,8 @@ const footerDivs = [
   {txt: 'Email', link: 'mailto:braedenmeikle@gmail.com', icon: 'fa fa-envelope', nav: false},
   {txt: 'LinkedIn', link: 'https://www.linkedin.com/in/bmeikle3/', icon: 'fa fa-linkedin-square', nav: false},
   {txt: 'GitHub', link: 'https://github.com/bmeikle56', icon: 'fa fa-github', nav: false},
-  {txt: 'Projects', link: 'https://braedenmeikle.dev/projects', icon: 'fa fa-folder', nav: true},
+  {txt: 'Projects', icon: 'fa fa-folder', nav: true},
 ]
-
-const iconStyle = {
-  fontSize: '12px',
-  color: txtCol
-}
 
 function Footer() {
   const navigate = useNavigate()
@@ -22,14 +17,15 @@ function Footer() {
       <div className='footer'>
         {footerDivs.map((footerDiv, i) => {
           return <div key={i} style={{display: 'flex', alignItems:'center'}}>
-            <i className={footerDiv.icon} style={iconStyle}></i>
+            {!footerDiv.nav && <i className={footerDiv.icon} style={{fontSize: '12px', color: txtCol}}/>}
+            {footerDiv.nav && <i className={footerDiv.icon} style={{fontSize: '12px', color: specialColor}}/>}
             {footerDiv.nav && 
-              <button style={{textDecoration:'none', background: 'transparent'}} onClick={() => navigate('/projects')}>
-                <p className='footer-txt' style={{paddingLeft:'4px'}}>{footerDiv.txt}</p>
+              <button style={{textDecoration:'none', background: 'transparent', border: 'none'}} onClick={() => navigate('/projects')}>
+                <p className='footer-txt' style={{color: specialColor}}>{footerDiv.txt}</p>
               </button>}
             {footerDiv.link ? 
-              <a rel='noreferrer' target='_blank' href={footerDiv.link} style={{textDecoration:'none'}}><p className='footer-txt' style={{paddingLeft:'4px'}}>{footerDiv.txt}</p></a>
-              : <p className='footer-txt'>{footerDiv.txt}</p>}
+              <a rel='noreferrer' target='_blank' href={footerDiv.link} style={{textDecoration:'none'}}><p className='footer-txt' style={{paddingLeft:'4px', color: 'gray'}}>{footerDiv.txt}</p></a>
+              : !footerDiv.nav && <p className='footer-txt' style={{color: 'gray'}}>{footerDiv.txt}</p>}
           </div>
         })}
         
