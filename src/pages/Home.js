33e90f204652595804ea8be3973f66628067c716
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-14 10:20:08 -0400

    Change diamonds to squares

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,8 @@ import { Loading } from '../components/Loading.js'
 import { motion } from 'framer-motion'
 
 function Wallpaper() {
-  return (
-    <div style={{display: 'flex', position: 'absolute', zIndex: 0, width: '100vw', height: '100vh'}}>
-      <DiamondWallpaper/>
-    </div>
-  )
-}
-
-function DiamondWallpaper() {
   const [screenSize, setScreenSize] = useState({ width: 0, height: 0 })
-  const diamondSize = 40 // width & height in pixels (includes padding)
+  const squareSize = 10
 
   useEffect(() => {
     const updateSize = () => {
@@ -27,24 +19,34 @@ function DiamondWallpaper() {
     return () => window.removeEventListener('resize', updateSize)
   }, [])
 
-  const cols = Math.ceil(screenSize.width / diamondSize)
-  const rows = Math.ceil(screenSize.height / diamondSize)
+  const cols = Math.ceil(screenSize.width / squareSize)
+  const rows = Math.ceil(screenSize.height / squareSize)
   const total = rows * cols
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${cols}, ${diamondSize}px)`,
-        gridTemplateRows: `repeat(${rows}, ${diamondSize}px)`,
-        width: '100vw',
-        height: '100vh',
-        zIndex: 1
-      }}
-    >
-      {Array.from({ length: total }).map((_, i) => (
-        <p key={i} style={{color: 'rgba(58, 0, 62, 1)', zIndex: 1}}>&diams;</p>
-      ))}
+    <div style={{display: 'flex', position: 'absolute', zIndex: 0, width: '100vw', height: '100vh'}}>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${cols}, ${squareSize}px)`,
+          gridTemplateRows: `repeat(${rows}, ${squareSize}px)`,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 1
+        }}
+      >
+        {Array.from({ length: total }).map((_, i) => (
+          <div
+            key={i}
+            style={{
+              width: '8px',
+              height: '8px',
+              backgroundColor: 'green',
+              borderRadius: '2px',
+              zIndex: 1
+            }}/>
+        ))}
+      </div>
     </div>
   )
 }
@@ -65,10 +67,8 @@ function Home() {
       >
         <Wallpaper/>
         <div style={{zIndex: 1, position: 'relative'}}>
-
-        
-        <Fun/>
-        <Footer/>
+          <Fun/>
+          <Footer/>
         </div>
       </motion.div>
     )
