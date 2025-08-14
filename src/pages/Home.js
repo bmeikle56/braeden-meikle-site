226312f255fdb0984684e67b305c90fb1af8b961
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-14 10:13:56 -0400

    Add a draft wallpaper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,53 @@
 import '../styles/global.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Fun } from '../components/Fun.js'
 import { Footer } from '../components/Footer.js'
 import { Loading } from '../components/Loading.js'
 import { motion } from 'framer-motion'
+
+function Wallpaper() {
+  return (
+    <div style={{display: 'flex', position: 'absolute', zIndex: 0, width: '100vw', height: '100vh'}}>
+      <DiamondWallpaper/>
+    </div>
+  )
+}
+
+function DiamondWallpaper() {
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 })
+  const diamondSize = 40 // width & height in pixels (includes padding)
+
+  useEffect(() => {
+    const updateSize = () => {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight })
+    }
+
+    updateSize();
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  const cols = Math.ceil(screenSize.width / diamondSize)
+  const rows = Math.ceil(screenSize.height / diamondSize)
+  const total = rows * cols
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${cols}, ${diamondSize}px)`,
+        gridTemplateRows: `repeat(${rows}, ${diamondSize}px)`,
+        width: '100vw',
+        height: '100vh',
+        zIndex: 1
+      }}
+    >
+      {Array.from({ length: total }).map((_, i) => (
+        <p key={i} style={{color: 'rgba(58, 0, 62, 1)', zIndex: 1}}>&diams;</p>
+      ))}
+    </div>
+  )
+}
 
 function Home() {
   const [isLoading, setLoading] = useState(true)
@@ -19,8 +63,13 @@ function Home() {
         delay: .25
       }}
       >
+        <Wallpaper/>
+        <div style={{zIndex: 1, position: 'relative'}}>
+
+        
         <Fun/>
         <Footer/>
+        </div>
       </motion.div>
     )
   }
